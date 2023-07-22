eaacbf0480369422b1d19295c3309a2891e5b6cc
Hardcorelevelingwarrior <nhuducminh123@gmail.com>
2023-07-23 04:42:10 +0700

    Finish chapter 6

--- exp/main.go
+++ exp/main.go
@@ -28,3 +28,10 @@ func main() {
 //Ex2 : Experiment with different data types
 //Ex3 - [HARD] Learn how to use nested data : access by key : .Favorite.food
 //Ex4 - [HARD] Create an if/else statement in your tem-plate
+
+//5.3.1 Ex1 - What does MVC stand for? 
+//M stand for model, v stand for views and c for controller
+//5.3.2 Ex2 - What is each layer of MVC responsible for?
+//Views to render the output, controller for determine what model to use or what template to use , model is the part where we code our logic
+//5.3.3 Ex3 - What are some benefits to using MVC?
+//To structure our code for better read and maintain
--- main.go
+++ main.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"fmt"
+	
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"lenslocked.com/main/views"
 )
+var (
+	homeView *views.View
+ 	contactView *views.View
+ 	faqView *views.View
+	signupView *views.View
+)
+
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome</h1>")
+	must(homeView.Render(w,nil))
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send an email "+
-		"to <a href=\"mailto:support@lenslocked.com\">"+
-		"support@lenslocked.com</a>.")
+	must(contactView.Render(w,nil))
 }
 
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
-	fmt.Fprint(w, "<h1>This is our faq page</h1>")
+	must(faqView.Render(w,nil))
+}
+func signup(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-type","text/html")
+	must(signupView.Render(w,nil))
 }
 
 func notfound404(w http.ResponseWriter, r *http.Request) {
@@ -30,15 +41,26 @@ func notfound404(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	homeView = views.NewView("bootstrap","views/home.gohtml")
+	contactView = views.NewView("bootstrap","views/contact.gohtml")
+	faqView = views.NewView("bootstrap","views/faq.gohtml")
+	signupView = views.NewView("bootstrap","views/signup.gohtml")
 	var h http.Handler = http.HandlerFunc(notfound404)
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup",signup)
 	r.NotFoundHandler = h
 	http.ListenAndServe(":3000", r)
 }
 
+// A helper function that panics on any error
+func must(err error) {
+	if err != nil {
+	panic(err)
+	}
+	}
 //Using other router package
 /*func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-type", "text/html")
@@ -67,3 +89,5 @@ func main() {
 	router.NotFound = h
 	http.ListenAndServe(":3000", router)
 }*/
+//6.6.1 Ex1 - Create an FAQ page with the Bootstrap layout
+//6.6.2 Ex2 - Update the navbar to link to the FAQ page
