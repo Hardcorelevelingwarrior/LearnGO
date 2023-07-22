147177d1c9d1767b31aa8460a4a3a1809d8fa00e
Hardcorelevelingwarrior <nhuducminh123@gmail.com>
2023-07-22 16:38:28 +0700

    Finish chapter 4 with all exercises

--- main.go
+++ main.go
@@ -3,19 +3,67 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "contact" {
+/*
+	func home(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "text/html")
+		fmt.Fprint(w, "<h1>Welcome</h1>")
+	}
+
+	func contact(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "text/html")
 		fmt.Fprint(w, "To get in touch, please send an email "+
 			"to <a href=\"mailto:support@lenslocked.com\">"+
 			"support@lenslocked.com</a>.")
 	}
+
+	func faq(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "text/html")
+		fmt.Fprint(w, "<h1>This is our faq page</h1>")
+	}
+
+	func notfound404(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "text/html")
+		fmt.Fprint(w, "<h1>404 This page not exist</h1>")
+	}
+
+	func main() {
+		var h http.Handler = http.HandlerFunc(notfound404)
+		r := mux.NewRouter()
+		r.HandleFunc("/", home)
+		r.HandleFunc("/contact", contact)
+		r.HandleFunc("/faq", faq)
+		r.NotFoundHandler = h
+		http.ListenAndServe(":3000", r)
+	}
+*/
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome</h1>")
+}
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:support@lenslocked.com\">"+
+		"support@lenslocked.com</a>.")
+}
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-type", "text/html")
+	fmt.Fprint(w, "<h1>This is our faq page</h1>")
+}
+func notfound404(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	fmt.Fprint(w, "<h1>404 This page not exist</h1>")
 }
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	var h http.Handler = http.HandlerFunc(notfound404)
+	router := httprouter.New()
+	router.GET("/", home)
+	router.GET("/contact", contact)
+	router.GET("/faq", faq)
+	router.NotFound = h
+	http.ListenAndServe(":3000", router)
 }
